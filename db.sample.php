1e83a1be7f449d1ed4e6723223ec5534162c3142
Gino Reyes <ginoreyes1151@gmail.com>
2025-06-21 00:55:26 +0800

    Add .gitignore, db.sample.php, and update README setup instructions

---
+++ db.sample.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$db   = 'your_database';
+$user = 'your_username';
+$pass = 'your_password';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("❌ Connection failed: " . $conn->connect_error);
+}
